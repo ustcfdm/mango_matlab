053c8b6e433318ec39249b4e9564767028f79002
Mango <fengmango@outlook.com>
2019-10-10 17:13:24 -0500

    New function MgMkdir(dirName, overwrite)

---
+++ file/MgMkdir.m
@@ -0,0 +1,22 @@
+function MgMkdir(dirName, overwrite)
+% MgMkdir(dirName, overwrite)
+% This function creates a directory called dirName. Arguments:
+% dirName: directory name.
+% overwrite: true/false, whether to overwrite if the directory already exists.
+
+if nargin < 2
+    overwrite = false;
+end
+
+if exist(dirName) == 7
+    if overwrite == true
+        rmdir(dirName, 's');
+    else
+        return;
+    end
+end
+
+mkdir(dirName);
+
+end
+
