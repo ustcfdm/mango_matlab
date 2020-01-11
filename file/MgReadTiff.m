da72cdf7c78d39bf58771d781e2ced51f957fed6
Mango <fengmango@outlook.com>
2020-01-11 17:20:49 -0600

    Add function MgReadTiff

---
+++ file/MgReadTiff.m
@@ -0,0 +1,28 @@
+function data = MgReadTiff(filename)
+% data = MgReadRawFile(filename)
+% This function reads tiff file as a volume. Arguments:
+% filename: the name of the file
+
+
+warning('off', 'imageio:tiffmexutils:libtiffWarning');
+
+t = Tiff(filename, 'r');
+info = imfinfo(filename);
+
+w = info(1).Width;
+h = info(1).Height;
+s = length(info);
+
+data = zeros(h, w, s);
+
+for k = 1:s
+    t.setDirectory(k);
+    data(:,:,k) = t.read();
+end
+
+t.close();
+
+warning('on', 'imageio:tiffmexutils:libtiffWarning');
+
+end
+
