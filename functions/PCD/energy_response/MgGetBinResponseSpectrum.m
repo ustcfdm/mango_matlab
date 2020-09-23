c226494dbcab757bfa493ed4cafb95619af28c4a
Mango Feng <fengmango@outlook.com>
2020-09-23 11:16:51 -0500

    Modify function MgGetBinResponseSpectrum

--- functions/PCD/energy_response/MgGetBinResponseSpectrum.m
+++ functions/PCD/energy_response/MgGetBinResponseSpectrum.m
@@ -1,12 +1,17 @@
-function bin_spectrum = MgGetBinResponseSpectrum(spectrum, energy, threshold_low, threshold_high)
+function bin_spectrum = MgGetBinResponseSpectrum(spectrum, energy, threshold_low, threshold_high, a, b)
 % For a given spectrum, return detected spectrum with detector energy response.
 % spectrum: array of photon counts.
 % energy: [keV].
 % threshold_low: low threshold [keV] (included).
 % threshold_high: high threshold [keV] (not included).
 % bin_spectrum: respond spectrum in energy bin [threshold_low,threshold_high)
+% a, b: shift coefficients of energy (a*E + b)
 
-enRes = importdata('MgEnergyResponseData.txt');
+if nargin <= 4
+    enRes = MgGetEnergyResponseMatrix();
+else
+    enRes = MgGetEnergyResponseMatrix(a, b);
+end
 
 en = interp1(squeeze(energy), squeeze(spectrum), 1:120);
 en(isnan(en)) = 0;
