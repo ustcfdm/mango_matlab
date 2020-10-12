64092c7a1793fa545af71c9df88c57bcb2a875ad
Mango <fengmango@outlook.com>
2020-10-12 17:50:55 -0500

    Update function MgSoftThresholdSegment

--- functions/image_process/MgSoftThresholdSegment.m
+++ functions/image_process/MgSoftThresholdSegment.m
@@ -1,9 +1,17 @@
-function [img_low, img_high] = MgSoftThresholdSegment(img, th_low, th_high)
+function [img_low, img_high] = MgSoftThresholdSegment(img, th_low, th_high, applyThreshold)
 % [img_low, img_high] = MgSoftThresholdSegment(img, th_low, th_high)
 % Segment images with soft threshold method.
 % img: image to be segmented (2D or 3D).
 % th_low: low threshold.
 % th_high: high threshold.
+% applyThreshold: (optional) true or false (default is false). 
+%                 If true, [img_low, img_high] will be segmented images. 
+%                 If false, [img_low, img_high] will ratio. Take product img.*img_low or img.*img_high to get final
+%                 segmented images.
+
+if nargin < 4
+    applyThreshold = false;
+end
 
 img_low = zeros(size(img));
 img_high = zeros(size(img));
@@ -13,12 +21,18 @@ idx_high = img > th_high;
 idx_middle = ~idx_low & ~idx_high;
 
 % low and high part
-img_low(idx_low) = img(idx_low);
-img_high(idx_high) = img(idx_high);
+img_low(idx_low) = 1;
+img_high(idx_high) = 1;
 
 % intermediate part
-img_high(idx_middle) = (img(idx_middle) - th_low) / (th_high - th_low)  .* img(idx_middle);
-img_low(idx_middle) = img(idx_middle) - img_high(idx_middle);
+img_high(idx_middle) = (img(idx_middle) - th_low) / (th_high - th_low);
+img_low(idx_middle) = 1 - img_high(idx_middle);
+
+
+if applyThreshold
+    img_low = img .* img_low;
+    img_high = img .* img_high;
+end
 
 
 end
