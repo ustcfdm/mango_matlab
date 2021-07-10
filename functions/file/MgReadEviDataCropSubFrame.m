07f37cf432f8153c6b30964d8dbfbab479ae17ed
Mango Feng <fengmango@outlook.com>
2021-07-10 18:49:17 -0500

    Add new function  MgReadEviDataCropSubFrame

---
+++ functions/file/MgReadEviDataCropSubFrame.m
@@ -0,0 +1,30 @@
+function img = MgReadEviDataCropSubFrame(filename, fullWidth, fullHeight, idxFrameBegin, frameCount)
+% Read EVI file data with crop. For example, the image is cropped from
+% 5120x64 to 3000x60. This function will read it as full size (5120x64).
+% The border will be set to 0.
+% filename: EVI file name.
+% fullWidth: full width (default: 5120).
+% fullHeight: full height (default: 64).
+% idxFrameBegin: index start from 1.
+% frameCount: number of frames to read.
+% img: output image.
+
+if nargin == 1
+    fullWidth = 5120;
+    fullHeight = 64;
+end
+
+
+info = MgReadEviInfo(filename);
+
+dataType = MgConvertEviDataTypeToMatlabDataType(info.Image_Type);
+
+img = zeros(fullHeight, fullWidth, frameCount);
+rows = (1:info.Height) + info.ROI_Y;
+cols = (1:info.Width) + info.ROI_X;
+
+img(rows, cols, :) = MgReadRawFileSubRegion(filename, info.Height, info.Width, info.Nr_of_images, info.Offset_To_First_Image, info.Gap_between_iamges_in_bytes, dataType, ...
+    [1, info.Height; 1, info.Width; idxFrameBegin, idxFrameBegin + frameCount - 1], false);
+
+end
+
