d65d6cd4c9b351eff9c033227f107b6d4c53106f
Mango Feng <fengmango@outlook.com>
2021-08-06 18:00:04 -0500

    Update CT_one_stop_procedure

---
+++ functions/ct/CT_one_stop_procedure/MgBenchtopDualEnergyDecomp.m
@@ -0,0 +1,90 @@
+function [obj_folder_pmma, obj_folder_Al] = MgBenchtopDualEnergyDecomp(config_filename)
+% Perform projection domain material decomposition and generate sinogram for benchtop PCD-CT system.
+
+js = MgReadJsoncFile(config_filename);
+
+%% folders to save decomp sinogram and img (w/ and w/o rebin)
+obj_folder_pmma = 'DE/PMMA_decomp';
+obj_folder_Al = 'DE/Al_decomp';
+
+folder_sgm_PMMA = sprintf('./sgm/%s/%s', js.ObjectName, obj_folder_pmma);
+folder_sgm_Al = sprintf('./sgm/%s/%s', js.ObjectName, obj_folder_Al);
+
+MgMkdir(folder_sgm_PMMA, false);
+MgMkdir(folder_sgm_Al, false);
+
+%% load panel correction parameters
+load(sprintf('%s/cali_DE.mat', js.PanelCorrParamFolder));
+cali_PMMA = cali_PMMA{2, 2};
+cali_Al = cali_Al{2, 2};
+
+%=========================================================
+% read files and do decomposition
+%=========================================================
+% read EVI air data
+file_air_LE = sprintf('./EVI/%s/air_LE.EVI', js.AirName);
+file_air_HE = sprintf('./EVI/%s/air_HE.EVI', js.AirName);
+if ~isfile(file_air_LE)
+    file_air_TE = sprintf('./EVI/%s/air_TE.EVI', js.AirName);
+    MgConvertHeTeToLe(file_air_TE, file_air_HE, file_air_LE);
+end
+prj_air_LE = mean(MgReadEviData(file_air_LE), 3);
+prj_air_HE = mean(MgReadEviData(file_air_HE), 3);
+
+% axial width after rebin
+widthRebin = size(prj_air_HE,2) / js.RebinSize;
+
+% obj file names
+[files_TE_short, files_TE_long] = MgDirRegExpV2(sprintf('./EVI/%s', js.ObjectName), sprintf('%s_TE.EVI', js.ObjectIndex));
+[files_HE_short, files_HE_long] = MgDirRegExpV2(sprintf('./EVI/%s', js.ObjectName), sprintf('%s_HE.EVI', js.ObjectIndex));
+
+for n = 1:numel(files_HE_short)
+    %-------------------------------------------
+    % Step 1: read EVI and take log
+    %-------------------------------------------
+    file_LE = strrep(files_HE_long{n}, '_HE.EVI', '_LE.EVI');
+    if ~isfile(file_LE)
+        MgConvertHeTeToLe(files_TE_long{n}, files_HE_long{n}, file_LE);
+    end
+    prj_LE = MgReadEviData(file_LE);
+    prj_HE = MgReadEviData(files_HE_long{n});
+    
+    % take log
+    prj_log_LE = log(prj_air_LE ./ prj_LE);
+    prj_log_LE(isnan(prj_log_LE)) = 0;
+    prj_log_LE(isinf(prj_log_LE)) = 0;
+    prj_log_HE = log(prj_air_HE ./ prj_HE);
+    prj_log_HE(isnan(prj_log_HE)) = 0;
+    prj_log_HE(isinf(prj_log_HE)) = 0;
+
+    %-------------------------------------------
+    % Step 2: do the DE decomposition
+    %-------------------------------------------
+    prj_PMMA = MgPolyvalTwoVariable(cali_PMMA, prj_log_LE, prj_log_HE);
+    prj_Al = MgPolyvalTwoVariable(cali_Al, prj_log_LE, prj_log_HE);
+    
+    prj_PMMA(isnan(prj_PMMA)) = 0;
+    prj_PMMA(isinf(prj_PMMA)) = 0;
+    prj_Al(isnan(prj_Al)) = 0;
+    prj_Al(isinf(prj_Al)) = 0;
+    
+    %-------------------------------------------
+    % Step 3: reslice to sinogram shape
+    %-------------------------------------------
+    sgm_PMMA = MgResliceProjectionToSinogram(prj_PMMA, js.SliceStartIdx, js.SliceThickness, js.SliceCount);
+    sgm_Al = MgResliceProjectionToSinogram(prj_Al, js.SliceStartIdx, js.SliceThickness, js.SliceCount);
+    
+    %-------------------------------------------
+    % Step 4: rebin sinogram
+    %------------------------------------------
+    sgm_PMMA_rebin = MgRebinSinogram(sgm_PMMA, js.RebinSize);
+    sgm_Al_rebin = MgRebinSinogram(sgm_Al, js.RebinSize);
+    
+    % save to file
+    filename = strrep(files_HE_short{n}, '_HE.EVI', sprintf('_%d-%d-%d.raw', widthRebin, js.Views, js.SliceCount));
+    MgSaveRawFile(sprintf('%s/sgm_%s', folder_sgm_PMMA, filename), sgm_PMMA_rebin);
+    MgSaveRawFile(sprintf('%s/sgm_%s', folder_sgm_Al, filename), sgm_Al_rebin);
+end
+
+end
+
---
+++ functions/ct/CT_one_stop_procedure/MgBenchtopForwardProjectImages.m
@@ -0,0 +1,50 @@
+function MgBenchtopForwardProjectImages(config_filename, obj_folder, delete_tmp_config_file)
+% Generate config file for mgfpj and perform forward projection.
+
+if nargin < 3
+    delete_tmp_config_file = false;
+end
+
+js = MgReadJsoncFile(config_filename);
+
+%============================================================
+% config parameters (folder name, geometry etc.)
+%============================================================
+fpj.InputDir = sprintf('./img/%s/%s', js.ObjectName, obj_folder);
+fpj.OutputDir = sprintf('./sgm/%s/%s', js.ObjectName, obj_folder);
+MgMkdir(fpj.OutputDir, false);
+
+fpj.InputFiles = sprintf('img_%s_%d-%d-%d.raw', js.ObjectIndex, js.ImageDimension, js.ImageDimension, js.SliceCount);
+fpj.OutputFilePrefix = '';
+fpj.OutputFileReplace = {'img_', 'sgm_', sprintf('%d-%d-%d', js.ImageDimension, js.ImageDimension, js.SliceCount), sprintf('%d-%d-%d', js.EviWidth/js.RebinSize, js.Views, js.SliceCount)};
+
+fpj.ImageDimension = js.ImageDimension;
+fpj.ImageSize = js.ImageSize;
+fpj.SliceCount = js.SliceCount;
+
+fpj.SourceIsocenterDistance = js.SourceIsocenterDistance;
+fpj.SourceDetectorDistance = js.SourceDetectorDistance;
+
+fpj.StartAngle = js.ImageRotation;
+fpj.DetectorElementCount = js.EviWidth/js.RebinSize;
+fpj.Views = js.Views;
+
+fpj.DetectorElementSize = 0.1 * js.RebinSize;
+fpj.DetectorOffcenter = js.DetectorOffcenter;
+
+%=================================================================
+% forward project images
+%=================================================================
+% tmp folder for config file
+folder_config = 'config_temp';
+MgMkdir(folder_config, false);
+
+MgSaveToJsonFile(fpj, 'config_temp/config_fpj.jsonc');
+!mgfpj config_temp/config_fpj.jsonc
+
+if delete_tmp_config_file
+    delete('./config_temp/config_fbp_tmp.jsonc');
+end
+
+end
+
