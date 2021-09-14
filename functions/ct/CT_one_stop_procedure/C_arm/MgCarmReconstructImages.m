22323782356b256a2aa5b2aa54ebbb920f8287aa
Mango Feng <fengmango@outlook.com>
2021-09-13 21:56:40 -0500

    Update function MgCarmReconstructImages

--- functions/ct/CT_one_stop_procedure/C_arm/MgCarmReconstructImages.m
+++ functions/ct/CT_one_stop_procedure/C_arm/MgCarmReconstructImages.m
@@ -1,8 +1,11 @@
-function folder_img = MgCarmReconstructImages(config_filename, obj_folder, delete_tmp_config_file)
+function folder_img = MgCarmReconstructImages(config_filename, obj_folder, usePmatrix, delete_tmp_config_file)
 % Generate config file for mgfbp and perform the reconstruction.
 
-if nargin < 3
+if nargin < 4
     delete_tmp_config_file = false;
+end
+if nargin < 3
+    usePmatrix = true;
 end
 
 js = MgReadJsoncFile(config_filename);
@@ -11,7 +14,11 @@ js = MgReadJsoncFile(config_filename);
 % config parameters (folder name, geometry etc.)
 %============================================================
 fbp.InputDir = sprintf('./sgm/%s/%s', js.ObjectName, obj_folder);
-fbp.OutputDir = sprintf('./img/%s/%s/', js.ObjectName, obj_folder);
+if usePmatrix
+    fbp.OutputDir = sprintf('./img/%s/%s/', js.ObjectName, obj_folder);
+else
+    fbp.OutputDir = sprintf('./img/%s/%s_no_pmatrix/', js.ObjectName, obj_folder);
+end
 MgMkdir(fbp.OutputDir, false);
 
 fbp.InputFiles = sprintf('sgm_%s_.*%d-%d-%d.raw', js.ObjectIndex, js.EviWidth / js.RebinSize, js.Views, js.SliceCount);
@@ -23,6 +30,7 @@ fbp.SinogramWidth = js.EviWidth / js.RebinSize;
 fbp.SinogramHeight = js.Views;
 fbp.Views = js.Views;
 fbp.SliceCount = js.SliceCount;
+fbp.TotalScanAngle = js.TotalScanAngle;
 
 fbp.DetectorElementSize = 0.1 * js.RebinSize;
 fbp.DetectorOffcenter = js.DetectorOffcenter;
@@ -39,12 +47,13 @@ fbp.(js.KernelName) = js.KernelParameter;
 %------------------------------------------
 % pmatrix information
 %------------------------------------------
-fbp.PMatrixFile = sprintf('%s/pmatrix_file.jsonc', js.PmatrixFolder);
-fbp.SDDFile = sprintf('%s/sdd_file.jsonc', js.PmatrixFolder);
-fbp.SIDFile = sprintf('%s/sid_file.jsonc', js.PmatrixFolder);
-fbp.DetectorOffCenterFile = sprintf('%s/offcenter_file.jsonc', js.PmatrixFolder);
-fbp.ScanAngleFile = sprintf('%s/scan_angle.jsonc', js.PmatrixFolder);
-
+if usePmatrix
+    fbp.PMatrixFile = sprintf('%s/pmatrix_file.jsonc', js.PmatrixFolder);
+    fbp.SDDFile = sprintf('%s/sdd_file.jsonc', js.PmatrixFolder);
+    fbp.SIDFile = sprintf('%s/sid_file.jsonc', js.PmatrixFolder);
+    fbp.DetectorOffCenterFile = sprintf('%s/offcenter_file.jsonc', js.PmatrixFolder);
+    fbp.ScanAngleFile = sprintf('%s/scan_angle.jsonc', js.PmatrixFolder);
+end
 %=================================================================
 % reconstruct images
 %=================================================================
